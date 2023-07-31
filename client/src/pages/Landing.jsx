05c24d596b5bc460b1effd61476a2de2f131a8e2
Bryan Arius <bryanarius@gmail.com>
2023-07-31 18:41:55 -0400

    Add Landing Page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,38 @@
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Link } from "react-router-dom";
+import main from "../assets/images/main.svg";
+import { Logo } from "../components";
+
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job<span> tracking </span>app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            perspiciatis laudantium, ea repudiandae ipsam necessitatibus sit
+            eligendi nostrum esse aliquid quam, ut tenetur fugit libero
+            accusamus repellendus enim possimus totam!
+          </p>
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+
+          <Link to='/login' className='btn'>
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
 };
+
 export default Landing;
