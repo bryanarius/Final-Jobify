e86f34cec87c76338267f36b588b0748824baa2b
Bryan Arius <bryanarius@gmail.com>
2023-08-01 02:16:25 -0400

    Add Register Page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,34 @@
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo, FormRow } from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to='/login'>Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='text' name='name' defaultValue='Bryan' />
+        <FormRow
+          type='text'
+          name='lastName'
+          labelText='Last Name'
+          defaultValue='Arius'
+        />
+        <FormRow type='text' name='location' defaultValue='Earth' />
+        <FormRow type='email' name='email' defaultValue='Bryan@gmail.com' />
+        <FormRow type='password' name='password' defaultValue='secret' />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Register;
