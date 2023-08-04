e42e4990be47ee06e55872fae31d18dd453dc386
Bryan Arius <bryanarius@gmail.com>
2023-08-04 18:49:53 -0400

    Add JSON Middleware and Utilize Morgan and Dotenv Node Packages

---
+++ server.js
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+import morgan from "morgan";
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  res.json({ message: "data received", data: req.body });
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}...`);
+});
