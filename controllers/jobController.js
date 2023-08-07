c87dc24076529f70f7887209285e2060b6ba1bd2
Bryan Arius <bryanarius@gmail.com>
2023-08-06 20:59:14 -0400

    Update Get All Jobs and Get Single Job Controller for DB

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -7,6 +7,7 @@ let jobs = [
 ];
 
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs });
 };
 
@@ -17,7 +18,7 @@ export const createJob = async (req, res) => {
 
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+  const job = await Job.findById(id);
   if (!job) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
