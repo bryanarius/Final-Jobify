2af813ffbd328d42f02b11d983d61a9672f94a85
Bryan Arius <bryanarius@gmail.com>
2023-08-10 07:22:21 -0400

    Make User Routes

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+import { StatusCodes } from "http-status-codes";
+import User from "../models/UserModel.js";
+import Job from "../models/JobModel.js";
+
+export const getCurrentUser = async (req, res) => {
+  res.status(StatusCodes.OK).json({ msg: "get current user" });
+};
+export const getApplicationStats = async (req, res) => {
+  res.status(StatusCodes.OK).json({ msg: "Application stats" });
+};
+export const updateUser = async (req, res) => {
+  res.status(StatusCodes.OK).json({ msg: "Update User" });
+};
