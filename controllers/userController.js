4bfcb698eeddc9afadb1bff5998ab1b5dd421e9e
Bryan Arius <bryanarius@gmail.com>
2023-08-14 03:16:56 -0400

    Add Get Application Stats Functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,19 @@ import User from "../models/UserModel.js";
 import Job from "../models/JobModel.js";
 
 export const getCurrentUser = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "get current user" });
+  const user = await User.findOne({ _id: req.user.userId });
+  const userWithoutPassword = user.toJSON();
+  res.status(StatusCodes.OK).json({ userWithoutPassword });
 };
 export const getApplicationStats = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "Application stats" });
+  const users = await User.countDocuments();
+  const jobs = await Job.countDocuments();
+  res.status(StatusCodes.OK).json({ users, jobs });
 };
 export const updateUser = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "Update User" });
+  const obj = { ...req.body };
+  delete obj.password;
+  console.log(obj);
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, req.body);
+  res.status(StatusCodes.OK).json({ msg: "user updated" });
 };
