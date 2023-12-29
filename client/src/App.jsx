c85efd6de60490c280215b66671b9f55ab809822
Bryan Arius <bryanarius@gmail.com>
2023-12-28 23:31:57 -0500

    Add Query Invalidation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -64,7 +64,7 @@ const router = createBrowserRouter([
       {
         path: "login",
         element: <Login />,
-        action: loginAction,
+        action: loginAction(queryClient),
       },
       {
         path: "dashboard",
@@ -85,7 +85,7 @@ const router = createBrowserRouter([
           {
             path: "profile",
             element: <Profile />,
-            action: profileAction,
+            action: profileAction(queryClient),
           },
           {
             path: "admin",
